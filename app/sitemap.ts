9c748382e39689d6571900328a5fc6955ad3cdd5
M0mahmoud <houdmohamed85@gmail.com>
2024-10-07 01:46:24 +0300

    Edit Sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,38 @@
 import { MetadataRoute } from "next";
 
+// const defaultLocale = "en" as const;
+// const locales = ["en", "ar"] as const;
+
+const host = "https://chatstranger.vercel.app";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    {
-      url: "https://chatstranger.vercel.app/",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
+    getEntry("/"),
+    getEntry("chat"),
+    getEntry("random"),
+    getEntry("login"),
   ];
+}
+
+function getEntry(pathname: string): MetadataRoute.Sitemap[number] {
+  return {
+    url: getUrl(pathname),
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 1,
+    //TODO: Not Work
+    // alternates: {
+    //   languages: Object.fromEntries(
+    //     locales.map((locale) => [locale, getUrl(pathname, locale)])
+    //   ),
+    // },
+  };
+}
+
+// function getUrl(pathname: string, locale: string) {
+//   return `${host}/${locale}${pathname === "/" ? "" : pathname}`;
+// }
+// Without Locale for now.
+function getUrl(pathname: string) {
+  return `${host}/${pathname === "/" ? "" : pathname}`;
 }
